b70a7fe3ced650c0fcf3439d9362c623b4566e64
Boltaboyev Sarvarbek <boltaboyev708@gmail.com>
2025-10-08 17:19:01 +0500

    set up create helloworld

---
+++ cpp/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Sarvar Boltaboyev on 08/10/25.
+//
+
+#include <iostream>
+ 
+int main() {
+    std::cout << "Hello, world" << std::endl;
+    std::cin.get();
+}
