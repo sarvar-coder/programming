842e172b50e3c721762ba3b3ad34140107ed271c
Boltaboyev Sarvarbek <boltaboyev708@gmail.com>
2025-10-09 08:22:12 +0500

    day1 ends

--- cpp/HelloWorld/HelloWorld/main.cpp
+++ cpp/HelloWorld/HelloWorld/main.cpp
@@ -6,12 +6,25 @@
 //
 
 #include <iostream>
-//
+using namespace std;
 void Log(const char* message);
 int multiply(int a, int b);
+//int binarySearch(vector<)
 //
 int main() {
-    Log("Hello, world");
-    multiply(34, 1);
-    std::cin.get();
+    float annualSalary;
+    cout << "Enter your annual salary";
+    cin >> annualSalary;
+    float monthlySalary = annualSalary / 12;
+    cout << "Your monthly salary is " << monthlySalary << endl;
+    float tenYears = annualSalary * 10;
+    cout << "You earn " << tenYears << " dollars in 10 years" << endl;
+    
+//     data types
+    int integer = 1232434;
+    char character = 'a';
+    bool isRich = false;
+    float height = 180.9;
+    string sth = "eiu";
+    
 }
